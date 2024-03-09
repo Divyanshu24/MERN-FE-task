4c779a1bdd903c2ba9116690f4cb2174cad152c6
Divyanshu <guptadivyanshu24@gmail.com>
2024-03-09 07:32:39 +0530

    home route

--- server/server.js
+++ server/server.js
@@ -87,5 +87,12 @@ app.get('/api/components/count', (req, res) => {
   }
 });
 
+app.get('/',(req,res)=>{
+  try {
+    res.status(200).json({message:"hello"})
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
